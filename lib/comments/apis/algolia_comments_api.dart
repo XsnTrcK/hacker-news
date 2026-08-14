01f302637ff3014f14934af8f8a6d802636d1eec
XsnTrcK <42541979+XsnTrcK@users.noreply.github.com>
2026-08-13 20:33:53 -0500

    Algolia Comment Fetch (#21)
    * Convert to algolia for pulling comments so that we are not making sizeOf(comments) calls
    
    * Address copilot comments

---
+++ lib/comments/apis/algolia_comments_api.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:hackernews/comments/apis/comments_api.dart';
+import 'package:hackernews/models/item.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart';
+
+class AlgoliaCommentsRetriever implements CommentsHandler {
+  static const _algoliaItemBase = 'https://hn.algolia.com/api/v1/items';
+
+  final Client _httpClient;
+  late Box<String> _commentsBox;
+
+  AlgoliaCommentsRetriever(this._httpClient);
+
+  @override
+  Future init({bool deleteBox = false}) async {
+    _commentsBox = await Hive.openBox<String>("comments");
+    if (deleteBox) {
+      await _commentsBox.deleteFromDisk();
+      _commentsBox = await Hive.openBox<String>("comments");
+    }
+  }
+
+  @override
+  CommentItem getComment(int commentId) {
+    if (!_commentsBox.containsKey(commentId)) {
+      throw Exception("Comment could not be found in the store");
+    } else {
+      return Item.fromJson(_commentsBox.get(commentId)!) as CommentItem;
+    }
+  }
+
+  @override
+  Future<List<CommentItem>> fetchComments(ItemWithKids itemWithKids) async {
+    final response = await _httpClient
+        .get(Uri.parse('$_algoliaItemBase/${itemWithKids.id}'));
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Algolia comments fetch failed for story ${itemWithKids.id}: HTTP ${response.statusCode}');
+    }
+    final Map<String, dynamic> storyNode = jsonDecode(response.body);
+    final children = (storyNode['children'] as List?) ?? [];
+
+    return children
+        .map((child) => _flatten(child as Map<String, dynamic>))
+        .toList();
+  }
+
+  CommentItem _flatten(Map<String, dynamic> node) {
+    final children = (node['children'] as List?) ?? [];
+    final comment = CommentItem(
+      node['id'] as int,
+      node['created_at_i'] as int,
+      (node['author'] as String?) ?? '',
+      ItemState(),
+      (node['text'] as String?) ?? '',
+      children.map((c) => (c as Map<String, dynamic>)['id'] as int).toList(),
+      node['parent_id'] as int,
+      false,
+      false,
+    );
+    _commentsBox.put(comment.id, jsonEncode(comment.toMap()));
+
+    for (final child in children) {
+      _flatten(child as Map<String, dynamic>);
+    }
+
+    return comment;
+  }
+
+  @override
+  Future updateComment(CommentItem comment) async {
+    _commentsBox.put(comment.id, jsonEncode(comment.toMap()));
+  }
+}
